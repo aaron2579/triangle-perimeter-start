592f1a47cd7ce106a46994b16a048c3e0b116eed
aaron2579 <90651669+aaron2579@users.noreply.github.com>
2022-09-21 12:19:25 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 // Triangle Perimeter Assignment Start Code
+
+// Determine Perimeter Button Click Event
+document.getElementById('btn').addEventListener('click', btnClicked);
+
+function btnClicked() {
+    // Get Vertices Inputs
+    let XA = +document.getElementById('X-A').value;
+    let YA = +document.getElementById('Y-A').value;
+    let XB = +document.getElementById('X-B').value;
+    let YB = +document.getElementById('Y-B').value;
+    let XC = +document.getElementById('X-C').value;
+    let YC = +document.getElementById('Y-C').value;
+
+    // Calculate Side Lengths
+    let AB = dist(XA, YA, XB, YB)
+    let AC = dist(XA, YA, XC, YC)
+    let BC = dist(XB, YB, XC, YC)
+
+    // Calculate & Output Side and Perimeter 
+    document.getElementById("AB-ans").innerHTML = AB
+    document.getElementById("AC-ans").innerHTML = AC
+    document.getElementById("BC-ans").innerHTML = BC
+    document.getElementById("perimeter-ans").innerHTML = AB+AC+BC
+}
+
+function dist(xone, yone, xtwo, ytwo) {
+    return Math.sqrt((xtwo - xone) ** 2 + (ytwo-yone) ** 2);
+}
